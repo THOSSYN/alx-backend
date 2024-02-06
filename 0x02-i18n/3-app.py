7237c67be58e3643b51967bed081e22a00f992b2
THOSSYN <alextosin5@gmail.com>
2024-02-06 19:35:46 +0100

    Added all required file for task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,25 +4,30 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', methods=['GET'])
 def index_page():
-    title = _("home_title")
-    message = _("home_header")
-    return render_template('3-index.html', title=title, message=message)
+    # title = _("home_title")
+    # message = _("home_header")
+    return render_template('3-index.html')
 
 if __name__ == '__main__':
     app.run()
