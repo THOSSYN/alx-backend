ddb4c45f913eb230103c221563f33a84afe7814a
THOSSYN <alextosin5@gmail.com>
2024-02-06 18:51:52 +0100

    Updated 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,8 +15,11 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app.config.from_object(Config)
+
+
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Get the languages choice of an application"""
     return request.accepted_languages.best_match(
             app.config['LANGUAGES']
