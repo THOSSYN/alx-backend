e62f27347f7081855cdf49a44e21e1d60289edfc
THOSSYN <alextosin5@gmail.com>
2024-02-06 18:42:34 +0100

    Updated 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,10 +7,12 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 @babel.localeselector
 def get_locale():
@@ -18,3 +20,15 @@ def get_locale():
     return request.accepted_languages.best_match(
             app.config['LANGUAGES']
             )
+
+
+@app.route('/', methods=['GET'])
+def index_page() -> str:
+    """Defines a route for an index page"""
+    title = "Welcome to Holberton"
+    message = "Hello world"
+    return render_template('2-index.html', title=title, message=message)
+
+
+if __name__ == '__main__':
+    app.run()
