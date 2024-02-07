f10f690755dfa2718eaf38fe74e14f53da2e702e
THOSSYN <alextosin5@gmail.com>
2024-02-07 12:43:16 +0100

    Added 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Sets up a locale marker"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Configures language marker"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Returns a locale object for any language"""
+    if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'])
+def index_page() -> str:
+    """Defines an index page"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
